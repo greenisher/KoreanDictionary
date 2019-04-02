728b57f24ffdf97d694d7404a6706cde39a157b5
bgreen2 <bgreen@and.digital>
2019-04-02 12:16:01 +0100

    added divs to index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {myArray} from './krArray'
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { myArray } from "./krArray";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-
-var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+var randomItem = myArray[Math.floor(Math.random() * myArray.length)];
 
 function tick() {
-    const element = (
-      <div class="center">
+  const element = (
+    <div class="center">
+      <div className="time">
         <h1>Hello, user!</h1>
         <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+      <div className="phrase">
         <h3>Your phrase of the day is {randomItem}</h3>
       </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-  }
-  
-  setInterval(tick, 1000);
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+setInterval(tick, 1000);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
